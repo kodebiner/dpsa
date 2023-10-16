999a8551bd6bc67dabc5d02a7c053656820e1448
ARBAATURRIZAL <63523333+ARBAATURRIZAL@users.noreply.github.com>
2023-10-16 18:54:02 +0700

    Deploy CI & Myth/Auth

---
+++ app/Config/Routes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Myth\Auth\Config;
+
+use CodeIgniter\Router\RouteCollection;
+use Myth\Auth\Config\Auth as AuthConfig;
+
+/** @var RouteCollection $routes */
+
+// Myth:Auth routes file.
+$routes->group('/', static function ($routes) {
+    // Load the reserved routes from Auth.php
+    $config         = config(AuthConfig::class);
+    $reservedRoutes = $config->reservedRoutes;
+
+    // Login/out
+    $routes->get($reservedRoutes['login'], 'Auth::login', ['as' => 'login']);
+    $routes->post($reservedRoutes['login'], 'Auth::attemptLogin');
+    $routes->get($reservedRoutes['logout'], 'Auth::logout', ['as' => 'logout']);
+
+    // Registration
+    $routes->get($reservedRoutes['register'], 'Auth::register', ['as' => 'register']);
+    $routes->post($reservedRoutes['register'], 'Auth::attemptRegister');
+
+    // Activation
+    $routes->get($reservedRoutes['activate-account'], 'Auth::activateAccount', ['as' => 'activate-account']);
+    $routes->get($reservedRoutes['resend-activate-account'], 'Auth::resendActivateAccount', ['as' => 'resend-activate-account']);
+
+    // Forgot/Resets
+    $routes->get($reservedRoutes['forgot'], 'Auth::forgotPassword', ['as' => 'forgot']);
+    $routes->post($reservedRoutes['forgot'], 'Auth::attemptForgot');
+    $routes->get($reservedRoutes['reset-password'], 'Auth::resetPassword', ['as' => 'reset-password']);
+    $routes->post($reservedRoutes['reset-password'], 'Auth::attemptReset');
+});
+
+$routes->group('/',['filter' => 'login'], function ($routes) {
+    $routes->get('', 'Home::index');
+});
