8442c279484af9879dc926499c0c5356028ca57a
-S3ME- <99646777+S3ME@users.noreply.github.com>
2023-11-06 21:44:29 +0700

    Create 2023-06-11-163157_CreateTempProject.php

---
+++ app/Database/Migrations/2023-06-11-163157_CreateTempProject.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTempProject extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'id'            => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'name'          => ['type' => 'varchar', 'constraint' => 255],
+            'brief'         => ['type' => 'varchar', 'constraint' => 255],
+            'status'        => ['type' => 'int', 'constraint' => 11],
+            'production'    => ['type' => 'int', 'constraint' => 11],
+            'clientid'      => ['type' => 'int', 'constraint' => 11],
+        ];
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('temp_project');
+    }
+
+    public function down()
+    {
+        $fields = ['firstname', 'lastname', 'parentid'];
+        $this->forge->dropColumn('users', $fields);
+    }
+}
