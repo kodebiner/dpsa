86d8a9f79596b72faf2e777677e1aae0dcfe39a2
Kosmas Mahendra <blindcozy@users.noreply.github.com>
2023-11-23 19:45:24 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -43,7 +43,6 @@ $routes->group('/', static function ($routes) {
 
 $routes->group('/',['filter' => 'login'], function ($routes) {
     $routes->get('', 'Home::index');
-    $routes->get('dashboard', 'Home::index');
     $routes->get('logedin', 'Home::logedin');
 });
 
