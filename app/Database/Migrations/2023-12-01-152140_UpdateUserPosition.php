204b57977a491e97fb8b7c7670cc1a843d453a14
Kosmas Mahendra <blindcozy@users.noreply.github.com>
2023-12-01 15:24:52 +0700

    update user position migration

--- app/Database/Migrations/2023-12-01-152140_UpdateUserPosition.php
+++ app/Database/Migrations/2023-12-01-152140_UpdateUserPosition.php
@@ -4,19 +4,19 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class UpdateUserJabatan extends Migration
+class UpdateUserPosition extends Migration
 {
     public function up()
     {
         $fields = [
-            'jabatan'         => ['type' => 'varchar', 'constraint' => 255],
+            'position'         => ['type' => 'VARCHAR', 'constraint' => 255],
         ];
         $this->forge->addColumn('users', $fields);
     }
 
     public function down()
     {
-        $fields = ['jabatan'];
+        $fields = ['position'];
         $this->forge->dropColumn('users', $fields);
     }
 }
