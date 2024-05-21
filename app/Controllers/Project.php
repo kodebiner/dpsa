59039f5ea6f5563fe903f063e19fda0b576992b2
S3ME <99646777+S3ME@users.noreply.github.com>
2024-05-21 15:13:57 +0700

    Update Project.php

--- app/Controllers/Project.php
+++ app/Controllers/Project.php
@@ -915,10 +915,10 @@ class Project extends BaseController
             }
 
             // RAB Data
+            dd($input['checked' . $id]);
             if (isset($input['checked' . $id])) {
                 foreach ($input['eqty' . $id] as $paketid => $mdls) {
                     foreach ($mdls as $mdlid => $qty) {
-                        dd($input['checked' . $id]);
                         if (isset($input['checked' . $id][$mdlid])) {
                             $rab = $RabModel->where('mdlid', $mdlid)->where('paketid', $paketid)->where('projectid', $id)->first();
                             if ((!empty($rab)) && ($rab['qty'] != $input['eqty' . $id][$paketid][$mdlid]) && $input['eqty' . $id][$paketid][$mdlid] != "0") {
